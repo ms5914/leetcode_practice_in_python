6f15195577a4133f7c24552e3955fadb59c860b6
ms5914 <55366357+ms5914@users.noreply.github.com>
2023-10-20 00:46:04 -0700

    Time: 31 ms (97.16%), Space: 16.3 MB (17.02%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.py
+++ 0075-sort-colors/0075-sort-colors.py
@@ -4,6 +4,7 @@ class Solution:
         Do not return anything, modify nums in-place instead.
         """
         #Dutch national flag problem - solved using two pointers
+        #zero_tracker on left, two_tracker on the right
         
         zero_tracker = i = 0
         two_tracker = len(nums)-1
@@ -19,6 +20,8 @@ class Solution:
             elif nums[i] == 2:
                 nums[two_tracker], nums[i] = nums[i], nums[two_tracker]
                 two_tracker-=1
+                
+#Remember this tricky part: you can see that the numbers before i index is always 0 or 1, so when we meet a 0 we only need to swap 0 to the left once and increase i. But when we meet a 2, after swapping, the current number at index i is 0/1/2, we still need to handle it.
         
                 
                 
