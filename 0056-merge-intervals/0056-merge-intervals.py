75352c7491cb8a676a9bd7321f1bd8bb1dc19857
ms5914 <55366357+ms5914@users.noreply.github.com>
2023-10-19 21:19:42 -0700

    Time: 130 ms (86.53%), Space: 21.1 MB (44.91%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.py
+++ 0056-merge-intervals/0056-merge-intervals.py
@@ -5,7 +5,6 @@ class Solution:
         result.append(intervals[0])
         
         for i in range(1, len(intervals)):
-            
             if not intervals[i][0] > result[-1][1]:
                 top_interval = result.pop()
                 result.append([min(intervals[i][0], top_interval[0]), max(intervals[i][1], top_interval[1]) ])
