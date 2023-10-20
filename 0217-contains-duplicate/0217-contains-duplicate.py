db655bba1b8035676113ead4a81a091eb91c5ae1
ms5914 <55366357+ms5914@users.noreply.github.com>
2023-10-20 00:55:44 -0700

    Time: 486 ms (32.83%), Space: 31.8 MB (38.04%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.py
+++ 0217-contains-duplicate/0217-contains-duplicate.py
@@ -6,12 +6,12 @@ class Solution:
         # counts = collections.Counter(nums)
         # return any(val>=2 for val in counts.values()) 
     
-        #Method 2 - Use soritng
-        nums.sort()
-        for i in range(len(nums)-1):
-            if nums[i] == nums[i + 1]:
-                return True
-        return False;
+        # #Method 2 - Use soritng
+        # nums.sort()
+        # for i in range(len(nums)-1):
+        #     if nums[i] == nums[i + 1]:
+        #         return True
+        # return False;
     
         #Method 3 - One liner python wizardry
         return len(set(nums)) < len(nums)
