413d833012a6c911e80c7532fa93ee0b5166a152
ms5914 <55366357+ms5914@users.noreply.github.com>
2023-11-12 17:12:06 -0800

    Time: 3328 ms (30.77%), Space: 35.8 MB (41.03%) - LeetHub

--- 0336-palindrome-pairs/0336-palindrome-pairs.py
+++ 0336-palindrome-pairs/0336-palindrome-pairs.py
@@ -1,41 +1,58 @@
 class Solution(object):
     def palindromePairs(self, words):
+        """
+        :type words: List[str]
+        :rtype: List[List[int]]
+        """
 
-        def all_valid_prefixes(word):
+        result = []
+        word_lookup = {word:i for i, word in enumerate(words)}
+
+        def find_valid_prefixes(word):
             valid_prefixes = []
             for i in range(len(word)):
                 if word[i:] == word[i:][::-1]:
                     valid_prefixes.append(word[:i])
             return valid_prefixes
-
-        def all_valid_suffixes(word):
+        
+        def find_valid_suffixes(word):
             valid_suffixes = []
-            for i in range(len(word)):
+            for i in range(len(word)-1, -1, -1):
                 if word[:i+1] == word[:i+1][::-1]:
-                    valid_suffixes.append(word[i + 1:])
+                    valid_suffixes.append(word[i+1:])
             return valid_suffixes
 
-        word_lookup = {word: i for i, word in enumerate(words)}
-        solutions = []
-
-        for word_index, word in enumerate(words):
+        
+        for i, word in enumerate(words):
             reversed_word = word[::-1]
 
-            # Build solutions of case #1. This word will be word 1.
-            if reversed_word in word_lookup and word_index != word_lookup[reversed_word]:
-                solutions.append([word_index, word_lookup[reversed_word]])
-
-            # Build solutions of case #2. This word will be word 2.
-            for suffix in all_valid_suffixes(word):
-                reversed_suffix = suffix[::-1]
-                if reversed_suffix in word_lookup:
-                    solutions.append([word_lookup[reversed_suffix], word_index])
-
-            # Build solutions of case #3. This word will be word 1.
-            for prefix in all_valid_prefixes(word):
+            #Case 1 : The reverse is already present. Need to ignore the same index result if the word is already palindrome and appears as the reverse word. 
+            if reversed_word in word_lookup and word_lookup[reversed_word] != i:
+                result.append([i,word_lookup[reversed_word] ])
+            
+            #Case 2 : For all valid prefixes, find if their reverse exist. (This is first word)
+            for prefix in find_valid_prefixes(word):
                 reversed_prefix = prefix[::-1]
                 if reversed_prefix in word_lookup:
-                    solutions.append([word_index, word_lookup[reversed_prefix]])
-
-        return solutions
+                    result.append([i, word_lookup[reversed_prefix]])
             
+            #Case 3 : For all valid suffixes if their reverse exist (this is second word)
+            for suffix in find_valid_suffixes(word):
+                reversed_suffix = suffix[::-1]
+                if reversed_suffix in word_lookup:
+                    result.append([word_lookup[reversed_suffix],i])
+
+        return result
+                
+
+
+        
+        
+        
+
+        
+
+
+
+
+        
