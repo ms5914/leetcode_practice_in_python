b9fb820b676f924b6608365bc7c92bb0f35a9b9d
ms5914 <55366357+ms5914@users.noreply.github.com>
2023-11-22 01:36:31 -0800

    Time: 1003 ms (79.91%), Space: 27.8 MB (18.55%) - LeetHub

--- 0912-sort-an-array/0912-sort-an-array.py
+++ 0912-sort-an-array/0912-sort-an-array.py
@@ -1,5 +1,7 @@
 class Solution:
     def sortArray(self, nums: List[int]) -> List[int]:
+        #Counting Sort because the range is comparatively less
+        
         result= []
 
         max_limit = pow(10,5)+1
@@ -14,3 +16,29 @@ class Solution:
                 result.append(i-5*pow(10,4))
 
         return result
+
+    
+    #Better one using the range of array(min and max elem) as a size for storing the counts
+
+# class Solution:
+#     def sortArray(self, nums: List[int]) -> List[int]:
+#         def counting_sort():
+#             # Create the counting hash map.
+#             counts = {}
+#             # Find the minimum and maximum values in the array.
+#             minVal, maxVal = min(nums), max(nums)
+#             # Update element's count in the hash map.
+#             for val in nums:
+#                 counts[val] = counts.get(val, 0) + 1
+
+#             index = 0
+#             # Place each element in its correct position in the array.
+#             for val in range(minVal, maxVal + 1, 1):
+#                 # Append all 'val's together if they exist.
+#                 while counts.get(val, 0) > 0:
+#                     nums[index] = val
+#                     index += 1
+#                     counts[val] -= 1
+
+#         counting_sort()
+#         return nums
