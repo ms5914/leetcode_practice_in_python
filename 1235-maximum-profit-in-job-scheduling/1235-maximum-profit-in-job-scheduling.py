305d3ca23b9eff14cf0d38f078bbe05209f9b8fe
ms5914 <55366357+ms5914@users.noreply.github.com>
2023-11-26 17:53:10 -0800

    Time: 772 ms (15.76%), Space: 36.5 MB (38.88%) - LeetHub

--- 1235-maximum-profit-in-job-scheduling/1235-maximum-profit-in-job-scheduling.py
+++ 1235-maximum-profit-in-job-scheduling/1235-maximum-profit-in-job-scheduling.py
@@ -1,36 +1,70 @@
+import heapq
 class Solution(object):
+    
+    #Method 3 : Using heap and sorting
     def jobScheduling(self, startTime, endTime, profit):
-        """
-        :type startTime: List[int]
-        :type endTime: List[int]
-        :type profit: List[int]
-        :rtype: int
-        """
-        n = len(profit)
-        self.maxsize = n
-        jobs = []
+        jobs = list(zip(startTime, endTime, profit))
+        jobs.sort(key = lambda x:x[0])
+        heap = []
+        for job in jobs:
+            heap.append((job[1], job[2]))
 
-        for i in range(n):
-            jobs.append((startTime[i], endTime[i], profit[i]))
-        jobs.sort(key = lambda x: x[0])
-        self.jobs = jobs
+        heapq.heapify(heap)
+        maxProfit = 0
+        for job in jobs:
+            while heap and job[0] >= heap[0][0]:
+                maxProfit = max(maxProfit, heapq.heappop(heap)[1])
+            heapq.heappush(heap, (job[1], job[2]+maxProfit))
 
-        max_profit = self.find_max_profit()
-        return max_profit
+        maxProfit = 0
+        while heap:
+            maxProfit = max(maxProfit, heapq.heappop(heap)[1])
+
+        return maxProfit
+        
+        
+            
+            
+            
+            
+        
+        
+    
+    
+    
+    # Method 2: DP dervied from recursion with memoization
+#     def jobScheduling(self, startTime, endTime, profit):
+#         """
+#         :type startTime: List[int]
+#         :type endTime: List[int]
+#         :type profit: List[int]
+#         :rtype: int
+#         """
+#         n = len(profit)
+#         self.maxsize = n
+#         jobs = []
+
+#         for i in range(n):
+#             jobs.append((startTime[i], endTime[i], profit[i]))
+#         jobs.sort(key = lambda x: x[0])
+#         self.jobs = jobs
+
+#         max_profit = self.find_max_profit()
+#         return max_profit
 
     
-    def find_max_profit(self):
-        profit = [0] * len(self.jobs) 
-        profit[len(self.jobs)-1] = self.jobs[len(self.jobs)-1][2]
+#     def find_max_profit(self):
+#         profit = [0] * len(self.jobs) 
+#         profit[len(self.jobs)-1] = self.jobs[len(self.jobs)-1][2]
         
-        for i in range(len(self.jobs)-2, -1, -1):
-            j = bisect.bisect_left(self.jobs, self.jobs[i][1], key=lambda x:x[0])
+#         for i in range(len(self.jobs)-2, -1, -1):
+#             j = bisect.bisect_left(self.jobs, self.jobs[i][1], key=lambda x:x[0])
             
-            if j==len(self.jobs):
-                profit[i] = max(self.jobs[i][2], profit[i+1])
-            else:
-                profit[i] = max(self.jobs[i][2]+profit[j], profit[i+1])
-        return profit[0]
+#             if j==len(self.jobs):
+#                 profit[i] = max(self.jobs[i][2], profit[i+1])
+#             else:
+#                 profit[i] = max(self.jobs[i][2]+profit[j], profit[i+1])
+#         return profit[0]
     
     #     Method 1 : Using memoization with recursion
 #     def jobScheduling(self, startTime: List[int], endTime: List[int], profit: List[int]) -> int:
@@ -60,3 +94,5 @@ class Solution(object):
     
     #heap based solution 
    # https://leetcode.com/problems/maximum-profit-in-job-scheduling/discuss/2002347/Very-Clearly-Explained-Heap-Solution-in-Python-Easy-to-Understand!!!
+
+
