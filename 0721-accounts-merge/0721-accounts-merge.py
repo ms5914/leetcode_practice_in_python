c2aa89f6ec79e9cac03f6ad1dc83286242807607
ms5914 <55366357+ms5914@users.noreply.github.com>
2023-12-01 11:36:42 -0800

    Time: 203 ms (49.51%), Space: 25.9 MB (16.08%) - LeetHub

--- 0721-accounts-merge/0721-accounts-merge.py
+++ 0721-accounts-merge/0721-accounts-merge.py
@@ -1,5 +1,6 @@
 class Solution:
     def accountsMerge(self, accounts: List[List[str]]) -> List[List[str]]:
+        
         result = []
         adj_list = defaultdict(list)
         visited = set()
@@ -9,6 +10,8 @@ class Solution:
             for email in account[2:]:
                 adj_list[firstemail].append(email)
                 adj_list[email].append(firstemail)
+                
+                #We create a star like structure for every account like all the emails in an account are connected to the first email in that account (bidirectionally) and eventually each account which has even 1 common email will become a part of a single connected components. The problem thus reduces to just finding the number of connected components in the graph and adding the name label to them. 
         
         def dfs(first_email,merged_emails):
             visited.add(first_email)
@@ -20,7 +23,7 @@ class Solution:
             
         for account in accounts:
             name = account[0]
-            if not account[1] in visited:
+            if not account[1] in visited: #We traverse all the firstemails in an account if they haven't been visited before. That means they are a new component. 
                 merged_emails = []
                 dfs(account[1], merged_emails)
                 merged_emails.sort()
