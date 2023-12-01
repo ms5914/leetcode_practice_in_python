865da46a25414db0e60cdd8ecd42ea201bcec7af
ms5914 <55366357+ms5914@users.noreply.github.com>
2023-11-30 21:12:09 -0800

    Time: 65 ms (14.78%), Space: 16.5 MB (10.89%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.py
+++ 0994-rotting-oranges/0994-rotting-oranges.py
@@ -6,17 +6,16 @@ class Solution:
         q = deque()
         min_minutes = 0
         
-        count = 0
+        fresh_count = 0
         for i in range(m):
             for j in range(n):
                 if grid[i][j] == 2:
                     q.append([i,j,0])
+                elif grid[i][j] == 1:
+                    fresh_count+=1
                     
-        
-        
         while q:
             i,j,minutes = q.popleft()
-            count+=1
             min_minutes = max(min_minutes, minutes)
             dx = [0, 0, 1, -1]
             dy = [1, -1, 0, 0]
@@ -25,13 +24,21 @@ class Solution:
                 new_x = i + dx[k]
                 new_y = j + dy[k]
                 if new_x>=0 and new_x<m and new_y>=0 and new_y<n and grid[new_x][new_y]==1:
+                    
+                    fresh_count-=1
                     grid[new_x][new_y] = 2
                     q.append([new_x, new_y, minutes+1])
-        
-        if  all (list(grid[p][q] in (0,2) for p in range(m) for q in range(n))):
+            
+        if fresh_count == 0:
             return min_minutes
         else:
             return -1
+        
+        # if  all (list(grid[p][q] in (0,2) for p in range(m) for q in range(n))):
+        #     return min_minutes
+        # else:
+        #     return -1
+        
         
         
         
