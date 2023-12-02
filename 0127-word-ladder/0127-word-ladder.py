abd98d84d4d0c5758ebd4f9b708cd124f7580c68
ms5914 <55366357+ms5914@users.noreply.github.com>
2023-12-01 18:57:20 -0800

    Time: 143 ms (64.09%), Space: 24.8 MB (7.45%) - LeetHub

---
+++ 0127-word-ladder/0127-word-ladder.py
@@ -0,0 +1,70 @@
+class Solution:
+    def ladderLength(self, beginWord: str, endWord: str, wordList: List[str]) -> int:
+        
+        reachable = defaultdict(list)
+        
+#         def is_reachable(word1, word2):
+#             if len(word1) != len(word2):
+#                 return False
+            
+#             differ = False
+#             for i in range(len(word1)):
+#                 if word1[i]!=word2[i]: 
+#                     if differ:
+#                         return False
+#                     else:
+#                         differ = True
+#             return True
+        
+        for word in wordList:
+            for j in range(len(word)):
+                reachable[word[:j]+"*"+word[j+1:]].append(word)
+        
+                
+            # for j in range(i+1, len(wordList)):
+            #     if is_reachable(wordList[i], wordList[j]):
+            #         reachable[wordList[i]].append(wordList[j])
+            #         reachable[wordList[j]].append(wordList[i])
+                    
+#         for i in range(len(wordList)):
+#             if is_reachable(beginWord, wordList[i]):
+#                 reachable[wordList[i]].append(beginWord)
+#                 reachable[beginWord].append(wordList[i])
+                
+                
+        if beginWord == endWord or not beginWord or not wordList or not endWord or not endWord in wordList:
+            return 0
+        
+        q = deque()
+        level = 0
+        visited = set()
+        q.append([beginWord, 1])
+        visited.add(beginWord)
+        
+        while q:
+            word, count = q.popleft()
+            if word == endWord:
+                return count
+            
+            for i in range(len(word)):
+                generic_key = word[:i]+"*"+word[i+1:]
+                for word1 in reachable[generic_key]:
+                    if word1 not in visited:
+                        visited.add(word1)
+                        q.append([word1, count+1])
+        
+        return 0
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+            
+                
+        
