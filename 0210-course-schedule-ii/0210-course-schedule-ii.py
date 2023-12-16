1cb5d374baf3602709b2bf1fabdb6c2cc87b168c
ms5914 <55366357+ms5914@users.noreply.github.com>
2023-12-15 16:18:55 -0800

    Time: 113 ms (13.77%), Space: 19.7 MB (9.54%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.py
+++ 0210-course-schedule-ii/0210-course-schedule-ii.py
@@ -4,36 +4,92 @@ class Solution:
         
         
         
-        adjList = defaultdict(list)
-        in_edges = defaultdict(int)
-        for course in range(numCourses):                #You forgot this and missed out on edge case. For ex. there are no depedencies. Then in_edges should be explicitly set zero
-            in_edges[course] = 0
+#         adjList = defaultdict(list)
+#         in_edges = defaultdict(int)
+#         for course in range(numCourses):                #You forgot this and missed out on edge case. For ex. there are no depedencies. Then in_edges should be explicitly set zero
+#             in_edges[course] = 0
             
         
-        for a,b in prerequisites:
-            adjList[b].append(a) #Directed graph
-            in_edges[a]+=1
+#         for a,b in prerequisites:
+#             adjList[b].append(a) #Directed graph
+#             in_edges[a]+=1
         
         
-        q = deque()
-        for course in range(numCourses):                    #You should loop for all possible courses and not just the adjacency list
-            if in_edges[course] == 0:
-                q.append(course)
+#         q = deque()
+#         for course in range(numCourses):                    #You should loop for all possible courses and not just the adjacency list
+#             if in_edges[course] == 0:
+#                 q.append(course)
+        
+#         result = []
+        
+#         while q:
+#             pre_req = q.popleft()
+#             result.append(pre_req)
+            
+#             for course in adjList[pre_req]:
+#                 in_edges[course]-=1
+#                 if in_edges[course] == 0:
+#                     q.append(course)
+        
+#         return result if len(result) == numCourses else [] 
+        
+        
+        #Solution 2: Can be done using recursion also. See Editorial
+        # The way DFS would work is that we would consider all possible paths stemming from A before finishing up the recursion for A and moving onto other nodes. All the nodes in the paths stemming from the node A would have A as an ancestor. The way this fits in our problem is, all the courses in the paths stemming from the course A would have A as a prerequisite.
         
         result = []
+        if numCourses == 0:
+            return result
         
-        while q:
-            pre_req = q.popleft()
-            result.append(pre_req)
-            
-            for course in adjList[pre_req]:
-                in_edges[course]-=1
-                if in_edges[course] == 0:
-                    q.append(course)
-        
-        return result if len(result) == numCourses else [] 
+        visited = set()
                 
+        adjList = defaultdict(list)
+        for edge in prerequisites:
+            adjList[edge[1]].append(edge[0])
             
+        has_cycle = False
+            
+        exploring = set()
+        
+        def dfs(prereq):
+            nonlocal has_cycle
+            if has_cycle: 
+                return -1
+            visited.add(prereq)
+            exploring.add(prereq)
+            for course in adjList[prereq]:
+                if course in exploring:
+                    has_cycle = True
+                if not course in visited: 
+                    dfs(course)
+            exploring.remove(prereq)
+            result.append(prereq)
+         
+        for course in range(numCourses):
+            if not course in visited:
+                ret_val = dfs(course)
+                if has_cycle:
+                    return []
+
+        if len(result) == numCourses:
+            return result[::-1]
+        else: return []
+        
+            
+        
+        
+        
+        
+            
+            
+            
+        
+        
+            
+        
+        
+        
+        
                 
                 
             
