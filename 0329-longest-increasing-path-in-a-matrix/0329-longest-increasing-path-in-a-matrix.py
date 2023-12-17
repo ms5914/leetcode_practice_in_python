0b8227dc322f5eb587bfb4db43f354e167033c89
ms5914 <55366357+ms5914@users.noreply.github.com>
2023-12-16 17:28:00 -0800

    Time: 511 ms (12.09%), Space: 21 MB (29.94%) - LeetHub

--- 0329-longest-increasing-path-in-a-matrix/0329-longest-increasing-path-in-a-matrix.py
+++ 0329-longest-increasing-path-in-a-matrix/0329-longest-increasing-path-in-a-matrix.py
@@ -7,17 +7,19 @@ class Solution:
         
         delta = [(1,0),(-1,0),(0,1),(0,-1)]
         visited = set()
+        dp = [[0 for i in range(200)] for j in range(200)]
         
-        @lru_cache(maxsize=None)
+        
         def dfs(i,j):
-            visited.add((i,j))
+            if dp[i][j]:
+                return dp[i][j]
             max_path = 1
             for k in range(len(delta)):
                 new_x = i+delta[k][0]
                 new_y = j+delta[k][1]
-                if new_x >= 0 and new_x < m and new_y >=0 and new_y <n and not (new_x, new_y) in visited and matrix[new_x][new_y]>matrix[i][j]:
+                if new_x >= 0 and new_x < m and new_y >=0 and new_y <n and matrix[new_x][new_y]>matrix[i][j]:
                     max_path = max(max_path,dfs(new_x, new_y)+1)
-            visited.remove((i, j))
+            dp[i][j] = max_path
             return max_path
         
         ans = 1
