d60028eaf752b5a17e8352ca24229936ba7ce7fc
ms5914 <55366357+ms5914@users.noreply.github.com>
2023-12-18 17:05:50 -0800

    Time: 51 ms (5.94%), Space: 16.4 MB (29.37%) - LeetHub

--- 0269-alien-dictionary/0269-alien-dictionary.py
+++ 0269-alien-dictionary/0269-alien-dictionary.py
@@ -5,22 +5,25 @@ class Solution:
         adjList = defaultdict(set)
         inEdges = defaultdict(int)
         
-        inEdges = {c : 0 for word in words for c in word}
+        inEdges = {c : 0 for word in words for c in word} #Remember to onitialize this for all letters in a word in words. 
         
+        
+        #for every neighboring word, deduce dependencies. Find the first different letter at an index in every adjacent word, that will give us the dependency. 
         for i in range(size-1):
             j = 0
             while j<len(words[i]) and j<len(words[i+1]) and words[i][j] == words[i+1][j]:
                 j+=1
             if j<len(words[i]) and j<len(words[i+1]):
-                if not words[i+1][j] in adjList[words[i][j]]:
+                if not words[i+1][j] in adjList[words[i][j]]: 
+                    #Updates adj list and inEdges only if for a particular dependency hasn't been counted before.This is important to remember. 
                     adjList[words[i][j]].add(words[i+1][j])
                     inEdges[words[i+1][j]]+=1
             
             elif len(words[i+1])<len(words[i]):
-                return ""
+                return ""  
+            #If the i+1 th word is a prefix of ith word, ordering is not possible. Remember this edge case.  
             
-        
-        
+        #It's a DAG and now we are just doing topological sort using Kahn's algo to find the lexicographical order in Alien dictionary
         result = []
         q = deque([c for c in inEdges if inEdges[c]==0 ])
         while q:
@@ -31,6 +34,7 @@ class Solution:
                 if inEdges[letter] == 0:
                     q.append(letter)
         
+        #We need to make sure that result has gotten all the letters otherwise there is a cycle. 
         return "".join(result) if len(result) == len(inEdges) else ""
             
             
