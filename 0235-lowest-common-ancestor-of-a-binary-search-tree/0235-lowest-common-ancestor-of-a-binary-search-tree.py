5c77d3cdee8fd5827919cadd56fd7f7843405bf8
ms5914 <55366357+ms5914@users.noreply.github.com>
2023-12-20 15:29:47 -0800

    Time: 63 ms (94.47%), Space: 21.2 MB (12.40%) - LeetHub

--- 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.py
+++ 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.py
@@ -7,17 +7,37 @@
 
 class Solution:
     def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+        
+        #This solution works also if the given tree is not a BST.
         def findLCA(root, p, q):
+            #If we are at a leaf node and we couldn't find either value
             if not root:
                 return None
+            
+            #If we found one value, that means there is a possibility that this is the lca, the parent call will decide
             if root.val == p.val or root.val == q.val:
                 return root
             left = findLCA(root.left, p, q)
             right = findLCA(root.right, p, q)
+            
+            #If one value is in left and other is on the right then this is the LCA
             if left and right:
                 return root
             return left or right   
-        res = findLCA(root, p, q)
+        
+        def findLCA_bst(root, p, q):
+            if not root:
+                return None
+            if p.val<root.val and q.val<root.val:
+                return findLCA_bst(root.left, p, q)
+            elif p.val>root.val and q.val>root.val:
+                return findLCA_bst(root.right, p, q)
+            else:
+                return root
+            
+        
+        
+        res = findLCA_bst(root, p, q)
         return res
     
         
