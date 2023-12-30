2e7656b34e987892180d9d1b724c0335cf9f77cd
ms5914 <55366357+ms5914@users.noreply.github.com>
2023-12-30 13:05:00 -0800

    Time: 54 ms (95.54%), Space: 21.2 MB (5.49%) - LeetHub

--- 0285-inorder-successor-in-bst/0285-inorder-successor-in-bst.py
+++ 0285-inorder-successor-in-bst/0285-inorder-successor-in-bst.py
@@ -11,6 +11,7 @@ class Solution:
         found=False
         prev = None
         
+        #If there's a right child of predecessor node p -> then the left most child of p.right is the answer (if p.right doesn't have a child, then p.right itself is the answer)
         if p.right:
             temp = p.right
             while temp.left:
