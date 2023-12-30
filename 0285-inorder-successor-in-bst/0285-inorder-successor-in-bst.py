84171b495b94a4ae58614f04b1bc909863da4b39
ms5914 <55366357+ms5914@users.noreply.github.com>
2023-12-30 13:11:03 -0800

    Time: 47 ms (99.12%), Space: 21.3 MB (5.49%) - LeetHub

--- 0285-inorder-successor-in-bst/0285-inorder-successor-in-bst.py
+++ 0285-inorder-successor-in-bst/0285-inorder-successor-in-bst.py
@@ -7,44 +7,64 @@
 
 class Solution:
     def inorderSuccessor(self, root: TreeNode, p: TreeNode) -> Optional[TreeNode]:
-        result_node = None
-        found=False
-        prev = None
-        
-        #Optimization
-        #If there's a right child of predecessor node p -> then the left most child of p.right is the answer (if p.right doesn't have a child, then p.right itself is the answer)
-        if p.right:
-            temp = p.right
-            while temp.left:
-                temp = temp.left
-            return temp
-        
+        successor = None
+        current = root
+        while current:
+            if current.val > p.val:
+                successor = current
+                current = current.left
+            else:
+                current = current.right
+        return successor
+                
         
         
         
+# Solution 2: Only for BST
+
         
-        def in_order(root):
-            nonlocal result_node
-            nonlocal found
-            nonlocal prev
-            if found:
-                return
-            if root is not None:
-                in_order(root.left)
-                if prev is not None and prev.val == p.val and not found: #you forgot "and not found" here. Because this part may still execute even if you have found the node in left subtree and then it will again use the same prev / the other alternative is to update the prev even if you have found the desired prev. Also another way is that we can get rid of "found" all together and can just use the result_node to determine if we have found a result_node or not. 
-                    result_node = root
-                    found = True
-                    #prev = root This is required if you are not using " and not found"
-                    return
-                prev = root
-                in_order(root.right)
         
-        in_order(root)
+# Solution 1 : Will work for all binary trees
+#         result_node = None
+#         found=False
+#         prev = None
+#         #Optimization
+#         #If there's a right child of predecessor node p -> then the left most child of p.right is the answer (if p.right doesn't have a child, then p.right itself is the answer)
+#         if p.right:
+#             temp = p.right
+#             while temp.left:
+#                 temp = temp.left
+#             return temp
         
-        if found and result_node:
-            return result_node
-        else:
-            return None
+        
+        
+        
+        
+#         def in_order(root):
+#             nonlocal result_node
+#             nonlocal found
+#             nonlocal prev
+#             if found:
+#                 return
+#             if root is not None:
+#                 in_order(root.left)
+#                 if prev is not None and prev.val == p.val and not found: #you forgot "and not found" here. Because this part may still execute even if you have found the node in left subtree and then it will again use the same prev / the other alternative is to update the prev even if you have found the desired prev. Also another way is that we can get rid of "found" all together and can just use the result_node to determine if we have found a result_node or not. 
+#                     result_node = root
+#                     found = True
+#                     #prev = root This is required if you are not using " and not found"
+#                     return
+#                 prev = root
+#                 in_order(root.right)
+        
+#         in_order(root)
+        
+#         if found and result_node:
+#             return result_node
+#         else:
+#             return None
+        
+        
+        
 
         
         
