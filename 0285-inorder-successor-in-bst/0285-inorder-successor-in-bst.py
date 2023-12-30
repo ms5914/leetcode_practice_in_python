22271511e47cb2934112ede54862c4f88bf48b87
ms5914 <55366357+ms5914@users.noreply.github.com>
2023-12-30 13:03:26 -0800

    Time: 126 ms (5.41%), Space: 21.3 MB (5.49%) - LeetHub

--- 0285-inorder-successor-in-bst/0285-inorder-successor-in-bst.py
+++ 0285-inorder-successor-in-bst/0285-inorder-successor-in-bst.py
@@ -10,6 +10,17 @@ class Solution:
         result_node = None
         found=False
         prev = None
+        
+        if p.right:
+            temp = p.right
+            while temp.left:
+                temp = temp.left
+            return temp
+        
+        
+        
+        
+        
         def in_order(root):
             nonlocal result_node
             nonlocal found
@@ -18,7 +29,7 @@ class Solution:
                 return
             if root is not None:
                 in_order(root.left)
-                if prev is not None and prev.val == p.val and not found: #you forgot "and not found" here. Because this part may still execute even if you have found the node in left subtree and then it will again use the same prev / the other alternative is to update the prev even if you have found the desired prev
+                if prev is not None and prev.val == p.val and not found: #you forgot "and not found" here. Because this part may still execute even if you have found the node in left subtree and then it will again use the same prev / the other alternative is to update the prev even if you have found the desired prev. Also another way is that we can get rid of "found" all together and can just use the result_node to determine if we have found a result_node or not. 
                     result_node = root
                     found = True
                     #prev = root This is required if you are not using " and not found"
