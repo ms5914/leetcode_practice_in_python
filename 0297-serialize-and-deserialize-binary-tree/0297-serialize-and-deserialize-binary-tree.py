db77dd3bc89cbe46599f2a9d8f54b663b917be79
ms5914 <55366357+ms5914@users.noreply.github.com>
2024-01-01 18:24:55 -0800

    Time: 163 ms (10.75%), Space: 21.2 MB (98.25%) - LeetHub

---
+++ 0297-serialize-and-deserialize-binary-tree/0297-serialize-and-deserialize-binary-tree.py
@@ -0,0 +1,83 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Codec:
+
+    def serialize(self, root):
+        """Encodes a tree to a single string.
+        
+        :type root: TreeNode
+        :rtype: str
+        """
+        if not root:
+            return ""
+        q = deque()
+        q.append(root)
+        encoded = []
+        while q:
+            node = q.popleft()
+            if not node:
+                encoded.append("None")
+            else:
+                encoded.append(str(node.val))
+                q.append(node.left)
+                q.append(node.right)
+        
+        print(encoded)
+        
+        return "#".join(encoded)
+                
+            
+
+    def deserialize(self, data):
+        """Decodes your encoded data to tree.
+        
+        :type data: str
+        :rtype: TreeNode
+        """
+        if not data:
+            return None
+        data = data.split("#")
+        print(data)
+        if not data:
+            return None
+        else:
+            q = deque()
+            index = 0
+            root = TreeNode(data[index])
+            q.append(root)
+            while q:
+                node = q.popleft()
+                if data[index+1] == "None":
+                    node.left = None
+                else:
+                    node.left = TreeNode(data[index+1])
+                    q.append(node.left)
+                
+                if data[index+2] == "None":
+                    node.right = None
+                else:
+                    node.right = TreeNode(data[index+2])
+                    q.append(node.right)
+                index+=2
+            
+            return root
+                
+                    
+                    
+                
+                
+                
+                
+                
+        
+        
+
+# Your Codec object will be instantiated and called as such:
+# ser = Codec()
+# deser = Codec()
+# ans = deser.deserialize(ser.serialize(root))
