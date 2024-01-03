f8af5f744a832ac94e1bf836ca4d2f3b29f30982
ms5914 <55366357+ms5914@users.noreply.github.com>
2024-01-03 13:21:42 -0800

    Time: 87 ms (91.73%), Space: 22.2 MB (68.85%) - LeetHub

--- 0297-serialize-and-deserialize-binary-tree/0297-serialize-and-deserialize-binary-tree.py
+++ 0297-serialize-and-deserialize-binary-tree/0297-serialize-and-deserialize-binary-tree.py
@@ -13,23 +13,40 @@ class Codec:
         :type root: TreeNode
         :rtype: str
         """
+        
         if not root:
             return ""
-        q = deque()
-        q.append(root)
-        encoded = []
-        while q:
-            node = q.popleft()
+        li = []
+        def dfs(node):
             if not node:
-                encoded.append("None")
-            else:
-                encoded.append(str(node.val))
-                q.append(node.left)
-                q.append(node.right)
+                li.append("None")
+                return
+            li.append(str(node.val))
+            dfs(node.left)
+            dfs(node.right)
+        dfs(root)
+        return "#".join(li)
+    
+            
         
-        print(encoded)
+#         Solution 1: Using queue
+#         if not root:
+#             return ""
+#         q = deque()
+#         q.append(root)
+#         encoded = []
+#         while q:
+#             node = q.popleft()
+#             if not node:
+#                 encoded.append("None")
+#             else:
+#                 encoded.append(str(node.val))
+#                 q.append(node.left)
+#                 q.append(node.right)
         
-        return "#".join(encoded)
+#         print(encoded)
+        
+#         return "#".join(encoded)
                 
             
 
@@ -39,33 +56,55 @@ class Codec:
         :type data: str
         :rtype: TreeNode
         """
+        
         if not data:
-            return None
-        data = data.split("#")
-        print(data)
-        if not data:
-            return None
-        else:
-            q = deque()
-            index = 0
-            root = TreeNode(data[index])
-            q.append(root)
-            while q:
-                node = q.popleft()
-                if data[index+1] == "None":
-                    node.left = None
-                else:
-                    node.left = TreeNode(data[index+1])
-                    q.append(node.left)
-                
-                if data[index+2] == "None":
-                    node.right = None
-                else:
-                    node.right = TreeNode(data[index+2])
-                    q.append(node.right)
-                index+=2
-            
+            return []
+        
+        li = data.split("#")
+        ind = 0
+        def dfs():
+            nonlocal ind
+            if li[ind] == "None":
+                return None
+            root = TreeNode(li[ind])
+            ind+=1
+            root.left = dfs()
+            ind+=1
+            root.right = dfs()
             return root
+        res = dfs()
+        return res
+            
+        
+        
+#       Solution 1: Using queue
+#         if not data:
+#             return None
+#         data = data.split("#")
+#         print(data)
+#         if not data:
+#             return None
+#         else:
+#             q = deque()
+#             index = 0
+#             root = TreeNode(data[index])
+#             q.append(root)
+#             while q:
+#                 node = q.popleft()
+#                 if data[index+1] == "None":
+#                     node.left = None
+#                 else:
+#                     node.left = TreeNode(data[index+1])
+#                     q.append(node.left)
+                
+#                 if data[index+2] == "None":
+#                     node.right = None
+#                 else:
+#                     node.right = TreeNode(data[index+2])
+#                     q.append(node.right)
+#                 index+=2
+            
+#             return root
                 
                     
                     
