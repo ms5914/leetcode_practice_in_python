b4a24d2d68b033146ebe5b6c13a1138f0d1476b7
ms5914 <55366357+ms5914@users.noreply.github.com>
2024-01-18 17:37:08 -0800

    Time: 36 ms (92.29%), Space: 17.6 MB (5.90%) - LeetHub

---
+++ 0046-permutations/0046-permutations.py
@@ -0,0 +1,19 @@
+class Solution:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        result=[]
+        n = len(nums)
+        res = nums
+        def find_permutations(res, cur_index):
+                if cur_index == n:
+                    result.append(list(res))
+                for i in range(cur_index,len(nums)):
+                    res[cur_index], res[i] = res[i], res[cur_index]
+                    find_permutations(res, cur_index+1)
+                    res[cur_index], res[i] = res[i], res[cur_index]
+        
+        find_permutations(res, 0)
+        return result
+
+                
+            
+        
