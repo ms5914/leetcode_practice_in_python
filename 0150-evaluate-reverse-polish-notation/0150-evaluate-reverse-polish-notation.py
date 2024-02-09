1333103da7eeadd0f76841ab173bb2e4ed30f903
ms5914 <55366357+ms5914@users.noreply.github.com>
2024-02-08 22:47:33 -0800

    Time: 60 ms (87.43%), Space: 17.2 MB (47.37%) - LeetHub

--- 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.py
+++ 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.py
@@ -3,29 +3,11 @@ class Solution:
         
         if not tokens:
             return 0
-        
         calc_stack = []
-        
-        def division(x,y):
-           # print(f"HERE I AM{x},{y}")
-
-            if (x>=0) ^ (y>=0):
-                if abs(x)%abs(y) == 0:
-                    return x//y
-                else:
-                    return (x//y)+1
-           # print(f"HERE I AM{x},{y}")
-            return x//y
                 
-                
-            
-        
-        
-        
-        operators = {'+': lambda x,y:x+y, '-': lambda x,y:x-y, '*': lambda x,y: x*y, '/': lambda x,y:division(x,y)}
+        operators = {'+': lambda x,y:x+y, '-': lambda x,y:x-y, '*': lambda x,y: x*y, '/': lambda x,y:int(x/y)}
         
         for token in tokens:
-           # print(calc_stack)
             if token in operators:
                 operand2, operand1 = calc_stack.pop(), calc_stack.pop()
                 calc_stack.append(operators[token](operand1, operand2))
