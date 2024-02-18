6c08cafdea1df2cfb6fc6d4378b574ea4607758e
ms5914 <55366357+ms5914@users.noreply.github.com>
2024-02-17 18:58:11 -0800

    Time: 667 ms (72.35%), Space: 30.3 MB (92.34%) - LeetHub

--- 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.py
+++ 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.py
@@ -1,6 +1,13 @@
 class Solution:
     def largestRectangleArea(self, heights: List[int]) -> int:
+        
+        #The idea is that for every bar we need to find what is the max rectangle that we can make for that bar. Essentially we need to find the first bar that has height less than the current bar from both sides. This is the perfect case for stacks (Next smaller element). We will maintain a montonic increasing stack. As soon as we get a height that is less than the bar height present at the top of the stack, that means, we have gotten the next smaller element of that top bar and already we have previous small height bar on the left in the stack. 
+        
+        #We will be storing indexes instead of heights for ease of programming and calculating widths. 
+        
         stack = []
+        
+        #We need to append this to calculate heights in case of first bar. Since it won't have a previous smaller bar. 
         stack.append(-1)
         max_area = -1*float("inf")
         for i,h in enumerate(heights):
@@ -10,12 +17,12 @@ class Solution:
                 while stack[-1] != -1 and heights[stack[-1]]>h:
                     current_index = stack.pop()
                     current_height = heights[current_index]
-                    
                     width = i-stack[-1]-1
                     max_area = max(max_area, width*current_height)
                 stack.append(i)
         
         
+        #Now in the stack we will have montonically increasing bar heights. This means that these bars doesn't have a next smaller element on right. So the width will be now be len(heights)-stack[-1]-1 for each popped height. 
         while stack[-1] != -1:
             current_index = stack.pop()
             current_height = heights[current_index]
