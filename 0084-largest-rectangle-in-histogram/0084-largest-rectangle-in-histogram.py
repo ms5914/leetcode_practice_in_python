4a415dc21964f913b457aec3f3755ef466fc894d
ms5914 <55366357+ms5914@users.noreply.github.com>
2024-02-19 20:00:06 -0800

    Time: 696 ms (39.34%), Space: 30.3 MB (92.35%) - LeetHub

--- 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.py
+++ 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.py
@@ -1,7 +1,31 @@
 class Solution:
     def largestRectangleArea(self, heights: List[int]) -> int:
         
-        #The idea is that for every bar we need to find what is the max rectangle that we can make for that bar. Essentially we need to find the first bar that has height less than the current bar from both sides. This is the perfect case for stacks (Next smaller element). We will maintain a montonic increasing stack. As soon as we get a height that is less than the bar height present at the top of the stack, that means, we have gotten the next smaller element of that top bar and already we have previous small height bar on the left in the stack. 
+        #Solution 2
+        #This can also be done via recursion. The idea is to find the minimum lenght bar in between start and end. Now the max rectangle will be either the full rectangle made using this small bar from start to end. 
+#This approach won't be optimized if the given heights array is sorted in either descending or ascending order. Because then we will not be making problems of n/2 or any fraction. It will be of the form T(n) = T(n-1)+cn which is O(n^2)
+        
+#         def find_max(start, end):
+#             if start>end:
+#                 return 0
+#             else:
+#                 min_height = float("inf")
+#                 min_index = -1
+#                 for i in range(start, end+1):
+#                     if heights[i] <= min_height:
+#                         min_height = heights[i]
+#                         min_index = i
+#                 return max(min_height*(end-start+1), find_max(start, min_index-1), find_max(min_index+1, end))
+        
+#         return find_max(0, len(heights)-1)
+        
+
+
+
+        
+        
+        
+ #The idea is that for every bar we need to find what is the max rectangle that we can make for that bar. Essentially we need to find the first bar that has height less than the current bar from both sides. This is the perfect case for stacks (Next smaller element). We will maintain a montonic increasing stack. As soon as we get a height that is less than the bar height present at the top of the stack, that means, we have gotten the next smaller element of that top bar and already we have previous small height bar on the left in the stack. 
         
         #We will be storing indexes instead of heights for ease of programming and calculating widths. 
         
