386b713a1c00d05601046760c345c78b93c6dc4f
ms5914 <55366357+ms5914@users.noreply.github.com>
2024-02-23 14:20:41 -0800

    Time: 73 ms (68.98%), Space: 21 MB (17.57%) - LeetHub

--- 0253-meeting-rooms-ii/0253-meeting-rooms-ii.py
+++ 0253-meeting-rooms-ii/0253-meeting-rooms-ii.py
@@ -1,24 +1,24 @@
 class Solution:
     def minMeetingRooms(self, intervals: List[List[int]]) -> int:
-        start_times = [interval[0] for interval in intervals]
-        start_times.sort()
-        end_times = [interval[1] for interval in intervals]
-        end_times.sort()
-
-        count = 0
-        res_count = 0
-         
-        i,j = 0, 0
-
-        while i<len(start_times) and j<len(end_times):
-            if start_times[i]<end_times[j]:
-                count+=1
-                i+=1
-            else: 
-                count-=1
-                j+=1
-            res_count=max(res_count, count)
         
-        return res_count
-
+        max_count = 0
+        mapp = defaultdict(int)
+        count = 0
+        
+        
+        for start, end in intervals:
+            mapp[start]+=1
+            mapp[end]-=1
+        
+        for key in sorted(mapp.keys()):
+            count+=mapp[key]
+            
+            max_count = max(max_count, count)
+        
+        return max_count
+            
+            
+            
+            
+            
         
