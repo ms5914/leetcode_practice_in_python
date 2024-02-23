1d4225031d5a75d44988b2216a71fd4c2dae857d
ms5914 <55366357+ms5914@users.noreply.github.com>
2024-02-23 14:25:33 -0800

    Time: 78 ms (42.64%), Space: 20.4 MB (50.64%) - LeetHub

--- 0253-meeting-rooms-ii/0253-meeting-rooms-ii.py
+++ 0253-meeting-rooms-ii/0253-meeting-rooms-ii.py
@@ -1,21 +1,34 @@
 class Solution:
     def minMeetingRooms(self, intervals: List[List[int]]) -> int:
         
-        max_count = 0
-        mapp = defaultdict(int)
-        count = 0
+ # Solution2 Using MinHeap 
+        heap = []
+        intervals.sort(key=lambda v: (v[0],v[1]))
+        result = 0
+        for start,end in intervals:
+            if not heap:
+                heapq.heappush(heap, end)
+            else:
+                while heap and heap[0]<=start:
+                    _ = heapq.heappop(heap)
+                heapq.heappush(heap, end)
+            result = max(result, len(heap))
+        return result
+ # Solution 1: Line Sweep (Easier and better)
+#         max_count = 0
+#         mapp = defaultdict(int)
+#         count = 0
+#         for start, end in intervals:
+#             mapp[start]+=1
+#             mapp[end]-=1
         
+#         for key in sorted(mapp.keys()):
+#             count+=mapp[key]
+#             max_count = max(max_count, count)
         
-        for start, end in intervals:
-            mapp[start]+=1
-            mapp[end]-=1
-        
-        for key in sorted(mapp.keys()):
-            count+=mapp[key]
-            
-            max_count = max(max_count, count)
-        
-        return max_count
+#         return max_count
+
+
             
             
             
