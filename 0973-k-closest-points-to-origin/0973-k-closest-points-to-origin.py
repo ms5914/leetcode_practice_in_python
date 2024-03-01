a15da683791eb6dc90fd25fe75c0778a6a4c87fb
ms5914 <55366357+ms5914@users.noreply.github.com>
2024-02-29 18:24:17 -0800

    Time: 3189 ms (5.84%), Space: 24.5 MB (5.67%) - LeetHub

--- 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.py
+++ 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.py
@@ -1,24 +1,95 @@
 import math
 import heapq
+import random
 class Solution:
     def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
         
-        k_closest_heap = []
-        result = []
-        def find_distance(c):
-            x, y = c[0], c[1]
+        
+        
+        #Approach 2: Quick select
+            
+        def find_distance_from_origin(coord):
+            x, y = coord[0], coord[1]
             return math.sqrt(pow(x,2)+pow(y,2))
         
-        for point in points:
-            heapq.heappush(k_closest_heap, (-1*find_distance(point), point))
-            if len(k_closest_heap)>k:
-                heapq.heappop(k_closest_heap)
+            
+       
+        dist_coord_list = []
+        for coord in points:
+            dist_coord_list.append((find_distance_from_origin(coord), coord))
+            
         
-        while k_closest_heap:
-            _, point = heapq.heappop(k_closest_heap)
-            result.append(point)
         
-        return result
+        def quick_sort(A, p,r,k):
+           
+            if p<=r:
+                if p==r:
+                    return [pair for dist, pair in A[:p+1]]
+                q = partition(A, p,r)
+                if q == k-1:
+                    return [pair for dist, pair in A[:q+1]]
+                elif q<k-1:
+                    return quick_sort(A, q+1, r,k)
+                else:
+                    return quick_sort(A,p,q-1,k)
+            
+                
+                
+            
+        def partition(A, p, r):
+             
+            rand_index = random.randint(p,r)
+            A[rand_index], A[r] = A[r], A[rand_index]
+            
+            
+            start, i = p,p
+            end = r
+            while i<=end:
+                if A[i][0]<A[r][0]:
+                    A[start], A[i] = A[i], A[start]
+                    start+=1
+                    i+=1
+                else:
+                    i+=1
+            A[start], A[r] = A[r], A[start]
+            return start
+        
+        return quick_sort(dist_coord_list, 0, len(dist_coord_list)-1, k)
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+        
+        #Approach1: Heap
+#         k_closest_heap = []
+#         result = []
+#         def find_distance(c):
+#             x, y = c[0], c[1]
+#             return math.sqrt(pow(x,2)+pow(y,2))
+        
+#         for point in points:
+#             heapq.heappush(k_closest_heap, (-1*find_distance(point), point))
+#             if len(k_closest_heap)>k:
+#                 heapq.heappop(k_closest_heap)
+        
+#         while k_closest_heap:
+#             _, point = heapq.heappop(k_closest_heap)
+#             result.append(point)
+        
+#         return result
+
+
+
+
+
+
+
                 
         
         
