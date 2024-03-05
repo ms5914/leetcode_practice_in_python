8c40c6d0027ba0e07169e947a9b14e73ff6f794b
ms5914 <55366357+ms5914@users.noreply.github.com>
2024-03-05 14:11:19 -0800

    Time: 137 ms (11.72%), Space: 17.1 MB (34.36%) - LeetHub

--- 0680-valid-palindrome-ii/0680-valid-palindrome-ii.py
+++ 0680-valid-palindrome-ii/0680-valid-palindrome-ii.py
@@ -12,10 +12,9 @@ class Solution:
                 i+=1
                 j-=1
             else:
-                count = 1
                 s1 = s[i+1:j+1]
                 s2 = s[i:j]
-                return all([s1[i]==s1[~i] for i in range(len(s1))]) or all(s2[i]==s2[~i] for i in range(len(s2)))
+                return all([s1[i]==s1[~i] for i in range(len(s1)//2)]) or all(s2[i]==s2[~i] for i in range(len(s2)//2))
  
                 
         return True
