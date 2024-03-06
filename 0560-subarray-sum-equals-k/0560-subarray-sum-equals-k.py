c98670071148a3f88f86873c993373b3ab39d13f
ms5914 <55366357+ms5914@users.noreply.github.com>
2024-03-05 18:24:20 -0800

    Time: 243 ms (19.57%), Space: 19.4 MB (95.26%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.py
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.py
@@ -1,13 +1,15 @@
 class Solution:
     def subarraySum(self, nums: List[int], k: int) -> int:
         hm = defaultdict(int)
-        curr_sum = 0
-        count = 0
-        hm[0]=1 #Remember this to count subarrays that start from index 0 and has the target sum already
-        for i, num in enumerate(nums):
-            curr_sum+=num
-            if curr_sum-k in hm:
-                count+=hm[curr_sum-k]
-            hm[curr_sum]+=1
-        return count
+        hm[0] = 1
 
+        count = 0
+        cumm_sum = 0
+        for i,n in enumerate(nums):
+            cumm_sum+=n
+            if cumm_sum-k in hm:
+                count+=hm[cumm_sum-k]
+            hm[cumm_sum]+=1 
+        return count 
+                
+        
