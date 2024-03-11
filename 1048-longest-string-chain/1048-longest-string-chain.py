9b5ea996d99a033fbac246489a60e02748ad5047
ms5914 <55366357+ms5914@users.noreply.github.com>
2024-03-10 18:31:14 -0700

    Time: 190 ms (32.85%), Space: 17 MB (59.38%) - LeetHub

---
+++ 1048-longest-string-chain/1048-longest-string-chain.py
@@ -0,0 +1,38 @@
+class Solution:
+    
+    def find_one_char_difference(self, word1, word2):
+        if len(word1)==len(word2)-1:
+            for i in range(len(word1)):
+                if word1[i]==word2[i]:
+                    continue
+                else:
+                    return word1[i:]==word2[i+1:]
+            return True
+        else:
+            return False
+        
+    def longestStrChain(self, words: List[str]) -> int:
+        if len(words)<=1:
+            return len(words)
+        
+        words.sort(key=lambda v: len(v))
+        
+        mapp = defaultdict(list)
+        mapp[len(words[0])].append((words[0],1))
+        result = 1
+        
+        
+        for index, value in enumerate(words[1:],1):
+            curr_length, curr_word = len(value), value
+            if curr_length-1 in mapp:
+                maximum = 1
+                possible_words = mapp[curr_length-1]
+                for seq,length in possible_words:
+                    if self.find_one_char_difference(seq, value):
+                        maximum = max(maximum, length+1)
+                result = max(result, maximum)   
+                mapp[(curr_length)].append((value, maximum))
+            else:
+                mapp[(curr_length)].append((value, 1))
+                
+        return result
