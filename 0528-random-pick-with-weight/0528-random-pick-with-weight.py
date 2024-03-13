616dfd0beab7599be3af47806e5c3741192ff0ea
ms5914 <55366357+ms5914@users.noreply.github.com>
2024-03-13 13:55:01 -0700

    Time: 136 ms (89.71%), Space: 21.4 MB (37.52%) - LeetHub

--- 0528-random-pick-with-weight/0528-random-pick-with-weight.py
+++ 0528-random-pick-with-weight/0528-random-pick-with-weight.py
@@ -3,10 +3,13 @@ class Solution:
 
     def __init__(self, w: List[int]):
         for i in range(1, len(w)):
+            
+            #Assume each weight as a bucket. And their are sequene of buckets with their end coordinates in w[i]. This is being achieved by prefix sums
             w[i] = w[i-1]+w[i]
         self.w = w
 
     def pickIndex(self) -> int:
+        #Chhose a random index and find the corresponding bucket index. The width of the bucket is acting as the probability. 
         index = bisect.bisect_left(self.w,rnd.randint(1, self.w[len(self.w)-1]))
         return index
         
