78abab84819f63dbef74b248214d73689086acbb
ms5914 <55366357+ms5914@users.noreply.github.com>
2024-03-19 00:25:49 -0700

    Time: 2869 ms (10.59%), Space: 48 MB (24.33%) - LeetHub

--- 0827-making-a-large-island/0827-making-a-large-island.py
+++ 0827-making-a-large-island/0827-making-a-large-island.py
@@ -72,7 +72,47 @@ class Solution:
         
                     
                 
-            
+            #Solution 2: Intelligent way of marking every island by a particular number Ex.
+
+# 0000
+# 2200
+# 0003
+#There are two islands marked with integers 2 and 3 and also record the number of elements in those islands in a dictionary. 
+
+
+
+# class Solution(object):
+#     def largestIsland(self, grid):
+#         N = len(grid)
+
+#         def neighbors(r, c):
+#             for nr, nc in ((r-1, c), (r+1, c), (r, c-1), (r, c+1)):
+#                 if 0 <= nr < N and 0 <= nc < N:
+#                     yield nr, nc
+
+#         def dfs(r, c, index):
+#             ans = 1
+#             grid[r][c] = index
+#             for nr, nc in neighbors(r, c):
+#                 if grid[nr][nc] == 1:
+#                     ans += dfs(nr, nc, index)
+#             return ans
+
+#         area = {}
+#         index = 2
+#         for r in xrange(N):
+#             for c in xrange(N):
+#                 if grid[r][c] == 1:
+#                     area[index] = dfs(r, c, index)
+#                     index += 1
+
+#         ans = max(area.values() or [0])
+#         for r in xrange(N):
+#             for c in xrange(N):
+#                 if grid[r][c] == 0:
+#                     seen = {grid[nr][nc] for nr, nc in neighbors(r, c) if grid[nr][nc] > 1}
+#                     ans = max(ans, 1 + sum(area[i] for i in seen))
+#         return ans
                 
             
         
