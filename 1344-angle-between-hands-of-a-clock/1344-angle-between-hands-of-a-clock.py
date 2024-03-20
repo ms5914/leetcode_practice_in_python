fc6011a68ef11a047a6a5ba2e50ee09794fee75a
ms5914 <55366357+ms5914@users.noreply.github.com>
2024-03-20 14:06:19 -0700

    Time: 33 ms (67.41%), Space: 16.6 MB (22.40%) - LeetHub

--- 1344-angle-between-hands-of-a-clock/1344-angle-between-hands-of-a-clock.py
+++ 1344-angle-between-hands-of-a-clock/1344-angle-between-hands-of-a-clock.py
@@ -4,6 +4,8 @@ class Solution:
         one_hour_angle = 30
         
         minutes_angle = one_min_angle * minutes
+        
+        #%12 is optional since the given format is 12 hr only
         hour_angle = (hour % 12 + minutes / 60) * one_hour_angle
         
         diff = abs(hour_angle - minutes_angle)
