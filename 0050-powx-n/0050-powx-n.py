b36605ee825c76e810b3568654debe755883d24c
ms5914 <55366357+ms5914@users.noreply.github.com>
2024-03-20 16:45:35 -0700

    Time: 29 ms (90.20%), Space: 16.5 MB (72.06%) - LeetHub

--- 0050-powx-n/0050-powx-n.py
+++ 0050-powx-n/0050-powx-n.py
@@ -1,6 +1,5 @@
 class Solution:
     def myPow(self, x: float, n: int) -> float:
-        
         if n == 0:
             return 1
         if n<0:
@@ -13,6 +12,7 @@ class Solution:
         while n>0:
             if n%2 == 1:
                 result*=x
+                n-=1
             x = x*x
             n = n//2
         return result
