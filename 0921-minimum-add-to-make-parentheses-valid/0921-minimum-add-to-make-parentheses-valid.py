00283715ac6a82f7cdedcefda921324b0524c83f
ms5914 <55366357+ms5914@users.noreply.github.com>
2024-03-20 17:03:23 -0700

    Time: 26 ms (96.37%), Space: 16.4 MB (99.27%) - LeetHub

--- 0921-minimum-add-to-make-parentheses-valid/0921-minimum-add-to-make-parentheses-valid.py
+++ 0921-minimum-add-to-make-parentheses-valid/0921-minimum-add-to-make-parentheses-valid.py
@@ -1,15 +1,26 @@
 class Solution:
     def minAddToMakeValid(self, s: str) -> int:
         st = []
-        count = 0
+        left_count = 0
+        right_count = 0
+        # for ch in s:
+        #     if ch == "(":
+        #         st.append(ch)
+        #     else:
+        #         if st:
+        #             st.pop()
+        #         else:
+        #             count+=1
+        # return count+len(st)
         for ch in s:
             if ch == "(":
-                st.append(ch)
+                left_count+=1
             else:
-                if st:
-                    st.pop()
+                if left_count>0:
+                    left_count-=1
                 else:
-                    count+=1
-        return count+len(st)
+                    right_count+=1
+        return left_count+right_count
+                
             
         
