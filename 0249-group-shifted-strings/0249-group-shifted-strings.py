a0d090f60ce479f860c5c7d88709917a267e65e2
ms5914 <55366357+ms5914@users.noreply.github.com>
2024-03-20 19:47:24 -0700

    Time: 46 ms (16.33%), Space: 16.6 MB (60.66%) - LeetHub

---
+++ 0249-group-shifted-strings/0249-group-shifted-strings.py
@@ -0,0 +1,11 @@
+class Solution:
+    def groupStrings(self, strings: List[str]) -> List[List[str]]:
+        mapp = defaultdict(list)
+        for string in strings:
+            curr = []
+            for a,b in zip(string, string[1:]):
+                remainder = (ord(b)-ord(a)+26)%26
+                curr.append(remainder)
+            mapp[tuple(curr)].append(string)
+        print(mapp)
+        return list(mapp.values())
