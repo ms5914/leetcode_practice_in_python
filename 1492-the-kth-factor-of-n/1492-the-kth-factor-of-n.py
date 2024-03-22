1b85d815ba2811e0134ed4b29a277cf76acdbd30
ms5914 <55366357+ms5914@users.noreply.github.com>
2024-03-22 08:02:57 -0700

    Time: 32 ms (79.96%), Space: 16.6 MB (24.11%) - LeetHub

--- 1492-the-kth-factor-of-n/1492-the-kth-factor-of-n.py
+++ 1492-the-kth-factor-of-n/1492-the-kth-factor-of-n.py
@@ -1,5 +1,7 @@
 class Solution:
     def kthFactor(self, n: int, k: int) -> int:
+        
+        #Check all factors before sqrt and see if k becomes zero
         n_sqrt = int(math.sqrt(n))
         for f in range(1, n_sqrt+1):
             if n%f == 0:
@@ -9,6 +11,7 @@ class Solution:
         
         factor_before_sqrt = n_sqrt if  n_sqrt*n_sqrt != n else n_sqrt-1
         
+        #Start going back the same factors, and return the counterpart factors if k == 0
         for f in range(factor_before_sqrt,0,-1):
             if n%f == 0:
                 k-=1
