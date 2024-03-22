82404378cfff514b27aeb6c5d4be7ff07da61587
ms5914 <55366357+ms5914@users.noreply.github.com>
2024-03-22 08:01:46 -0700

    Time: 34 ms (71.63%), Space: 16.6 MB (24.11%) - LeetHub

---
+++ 1492-the-kth-factor-of-n/1492-the-kth-factor-of-n.py
@@ -0,0 +1,21 @@
+class Solution:
+    def kthFactor(self, n: int, k: int) -> int:
+        n_sqrt = int(math.sqrt(n))
+        for f in range(1, n_sqrt+1):
+            if n%f == 0:
+                k-=1
+            if k==0:
+                return f
+        
+        factor_before_sqrt = n_sqrt if  n_sqrt*n_sqrt != n else n_sqrt-1
+        
+        for f in range(factor_before_sqrt,0,-1):
+            if n%f == 0:
+                k-=1
+            if k == 0:
+                return n//f
+        
+        return -1
+            
+            
+        
