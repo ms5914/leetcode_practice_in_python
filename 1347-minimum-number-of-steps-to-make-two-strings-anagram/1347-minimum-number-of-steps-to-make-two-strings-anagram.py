8676b01be31a0288e8af57860bc8544e544691ab
ms5914 <55366357+ms5914@users.noreply.github.com>
2024-03-29 00:41:49 -0700

    Time: 108 ms (76.31%), Space: 17.4 MB (9.48%) - LeetHub

--- 1347-minimum-number-of-steps-to-make-two-strings-anagram/1347-minimum-number-of-steps-to-make-two-strings-anagram.py
+++ 1347-minimum-number-of-steps-to-make-two-strings-anagram/1347-minimum-number-of-steps-to-make-two-strings-anagram.py
@@ -1,8 +1,5 @@
 class Solution:
     def minSteps(self, s: str, t: str) -> int:
-        print(Counter(s))
-        print(Counter(t))
-        print(Counter(s)-Counter(t))
         return sum((Counter(s) - Counter(t)).values())
 
         
