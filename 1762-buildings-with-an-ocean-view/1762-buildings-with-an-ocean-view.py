6cf6c3905cd2ad1bbab9af19597285e4324954c8
ms5914 <55366357+ms5914@users.noreply.github.com>
2024-04-06 18:45:42 -0700

    Time: 524 ms (36.78%), Space: 34 MB (42.18%) - LeetHub

--- 1762-buildings-with-an-ocean-view/1762-buildings-with-an-ocean-view.py
+++ 1762-buildings-with-an-ocean-view/1762-buildings-with-an-ocean-view.py
@@ -9,3 +9,20 @@ class Solution:
                     st.pop()
                 st.append(i)
         return st
+    
+        #Better approach without stack, just go from reverse and keep a max_height to check if current building is blocked or not.
+#         n = len(heights)
+#         answer = []
+#         max_height = -1
+        
+#         for current in reversed(range(n)):
+#             # If there is no building higher (or equal) than the current one to its right,
+#             # push it in the answer array.
+#             if max_height < heights[current]:
+#                 answer.append(current)
+            
+#                 # Update max building till now.
+#                 max_height = heights[current]
+        
+#         answer.reverse()
+#         return answer
