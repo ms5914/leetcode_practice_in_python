8c2304df6a61289caf051860f1f4310861265085
ms5914 <55366357+ms5914@users.noreply.github.com>
2024-04-06 19:19:17 -0700

    Time: 71 ms (98.13%), Space: 19.2 MB (80.92%) - LeetHub

--- 0227-basic-calculator-ii/0227-basic-calculator-ii.py
+++ 0227-basic-calculator-ii/0227-basic-calculator-ii.py
@@ -1,46 +1,27 @@
 class Solution:
     def calculate(self, s: str) -> int:
-        i = 0
-        s = s+")"
-        num = 0
-        sign = '+'
-        stack = []
-        result = 0
-        while i<len(s):
-            # print(stack)
-            # print(num)
-            # print(sign)
-            if s[i].isdigit():
-                while i<len(s) and s[i].isdigit():
-                    num = num*10+int(s[i])
-                    i+=1
-            elif s[i] in ('+', '-', '*', '/', ')'):
-                if sign in ('+', '-'):
-                    new_sign = 1 if sign == '+' else -1
-                    stack.append(new_sign*num)
-                    num = 0
-                    sign = s[i]
-                    i+=1
+        pre, cur, ans, sign = 0, 0, 0, "+"
+        s += "$"
+        for c in s:
+            if c.isdigit():
+                cur = cur * 10 + int(c)
+            elif c in "+-*/$":
+                if sign == "+":
+                    ans += pre
+                    pre = cur
+                elif sign == "-":
+                    ans += pre
+                    pre = -cur
+                elif sign == "*":
+                    pre *= cur
                 else:
-                    num1 = stack.pop()
-                    num2 = num
-                    if sign == "*":
-                        stack.append(num1*num2)
-                    else:
-                        stack.append(int(num1/num2))
-                    sign = s[i]
-                    i+=1
-                    num = 0
-            elif s[i] == " ":
-                i+=1
-        while stack:
-            elem = stack.pop()
-            result+=elem
-        
-        return result
-            
-            
-            
-            
-            
+                    pre = int(pre / float(cur))
+                cur = 0
+                sign = c
+        return ans + pre
+    
+
+    
+    
+    
         
