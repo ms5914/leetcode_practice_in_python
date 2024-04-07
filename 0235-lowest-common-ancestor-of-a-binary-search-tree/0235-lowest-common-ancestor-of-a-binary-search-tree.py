733409811c8dbf9fada7d073ed53fce299acebff
ms5914 <55366357+ms5914@users.noreply.github.com>
2024-04-06 18:37:08 -0700

    Time: 60 ms (27.55%), Space: 20.2 MB (85.22%) - LeetHub

--- 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.py
+++ 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.py
@@ -6,39 +6,40 @@
 #         self.right = None
 
 class Solution:
-    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
-        
-        #This solution works also if the given tree is not a BST.
-        def findLCA(root, p, q):
-            #If we are at a leaf node and we couldn't find either value
-            if not root:
-                return None
-            
-            #If we found one value, that means there is a possibility that this is the lca, the parent call will decide
-            if root.val == p.val or root.val == q.val:
-                return root
-            left = findLCA(root.left, p, q)
-            right = findLCA(root.right, p, q)
-            
-            #If one value is in left and other is on the right then this is the LCA
-            if left and right:
-                return root
-            return left or right   
-        
-        #This one uses the ordering present in a BST
-        def findLCA_bst(root, p, q):
-            if not root:
-                return None
-            if p.val<root.val and q.val<root.val:
-                return findLCA_bst(root.left, p, q)
-            elif p.val>root.val and q.val>root.val:
-                return findLCA_bst(root.right, p, q)
+    def lowestCommonAncestor(self, root, p, q):
+        """
+        :type root: TreeNode
+        :type p: TreeNode
+        :type q: TreeNode
+        :rtype: TreeNode
+        """
+
+        # Value of p
+        p_val = p.val
+
+        # Value of q
+        q_val = q.val
+
+        # Start from the root node of the tree
+        node = root
+
+        # Traverse the tree
+        while node:
+
+            # Value of current node or parent node.
+            parent_val = node.val
+
+            if p_val > parent_val and q_val > parent_val:    
+                # If both p and q are greater than parent
+                node = node.right
+            elif p_val < parent_val and q_val < parent_val:
+                # If both p and q are lesser than parent
+                node = node.left
             else:
-                return root
-            
+                # We have found the split point, i.e. the LCA node.
+                return node
         
         
-        res = findLCA_bst(root, p, q)
-        return res
-    
+        
+        
         
