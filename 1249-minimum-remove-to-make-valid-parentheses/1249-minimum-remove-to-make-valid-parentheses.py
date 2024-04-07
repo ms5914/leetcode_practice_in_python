9f0f30d9a7ebf75595f2bd399f7b8010c4dd2779
ms5914 <55366357+ms5914@users.noreply.github.com>
2024-04-06 18:16:20 -0700

    Time: 96 ms (24.98%), Space: 18.9 MB (17.18%) - LeetHub

--- 1249-minimum-remove-to-make-valid-parentheses/1249-minimum-remove-to-make-valid-parentheses.py
+++ 1249-minimum-remove-to-make-valid-parentheses/1249-minimum-remove-to-make-valid-parentheses.py
@@ -2,7 +2,7 @@ class Solution:
     def minRemoveToMakeValid(self, s: str) -> str:
         i = 0
         st = []
-        discard_index = set()
+        #discard_index = set()
         while i<len(s):
             if s[i] == '(':
                 st.append(i)
@@ -16,17 +16,34 @@ class Solution:
                 else:
                     st.append(i)
             i+=1
-        
-        for elem in st:
-            discard_index.add(elem)
-        
+            
+        i, j = 0, 0
         result = []
-        
-        for i in range(len(s)):
-            if i not in discard_index:
-                result.append(s[i])
+        while i<len(s) and j<len(st):
+            if i != st[j]:
+                result.append(s[i])   
+            else:
+                j = j+1
+            i=i+1
+            
+        while i<len(s):
+            result.append(s[i])
+            i+=1
         
         return "".join(result)
+                
+            
+        
+        # for elem in st:
+        #     discard_index.add(elem)
+        
+#         result = []
+        
+#         for i in range(len(s)):
+#             if i not in discard_index:
+#                 result.append(s[i])
+        
+#         return "".join(result)
             
             
                         
