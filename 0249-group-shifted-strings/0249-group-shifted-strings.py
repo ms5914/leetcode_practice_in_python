7270cc864deb1cb9da4c0b565e06a0eacb338ab0
ms5914 <55366357+ms5914@users.noreply.github.com>
2024-04-08 16:17:43 -0700

    Time: 42 ms (40.31%), Space: 17.2 MB (6.45%) - LeetHub

--- 0249-group-shifted-strings/0249-group-shifted-strings.py
+++ 0249-group-shifted-strings/0249-group-shifted-strings.py
@@ -1,16 +1,19 @@
 class Solution:
     def groupStrings(self, strings: List[str]) -> List[List[str]]:
-        mapp = defaultdict(list)
+        hm = defaultdict(list)
         
-        #For each string, create a key such that it captures difference between each consectibe letter in the string. 
-        #Then strings having the same key will belong to the same group. 
-        for string in strings:
-            curr = []
-            for a,b in zip(string, string[1:]):
-                remainder = (ord(b)-ord(a))%26
-                curr.append(remainder)
-            mapp[tuple(curr)].append(string)
-        return list(mapp.values())
-    
-    
-   
+        for word in strings:
+            prev = word[0]
+            key = ["1"]
+            for ch in word[1:]:
+                curr = ch
+                key.append(str((ord(curr)-ord(prev))%26))
+                prev = curr
+            #print(key)
+            hm[tuple(key)].append(word)
+        
+        return hm.values()
+            
+            
+                
+        
