b7632014f8e31efe5b385179d2868bfe3555927c
ms5914 <55366357+ms5914@users.noreply.github.com>
2024-04-09 22:21:59 -0700

    Time: 51 ms (90.61%), Space: 22.5 MB (45.27%) - LeetHub

--- 0173-binary-search-tree-iterator/0173-binary-search-tree-iterator.py
+++ 0173-binary-search-tree-iterator/0173-binary-search-tree-iterator.py
@@ -1,67 +1,45 @@
 # Definition for a binary tree node.
 # class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
 class BSTIterator:
 
-    def __init__(self, root: Optional[TreeNode]):
-        self.head = self.curr = TreeNode(-1)
-        self.inorder_traversal(root)
-        
-    def inorder_traversal(self, node):
-        if not node:
-            return
-        else:
-            self.inorder_traversal(node.left)
-            self.curr.right = node
-            self.curr = self.curr.right
-            self.inorder_traversal(node.right)
-            
-        
+    def __init__(self, root: TreeNode):
+
+        # Stack for the recursion simulation
+        self.stack = []
+
+        # Remember that the algorithm starts with a call to the helper function
+        # with the root node as the input
+        self._leftmost_inorder(root)
+
+    def _leftmost_inorder(self, root):
+
+        # For a given node, add all the elements in the leftmost branch of the tree
+        # under it to the stack.
+        while root:
+            self.stack.append(root)
+            root = root.left
 
     def next(self) -> int:
-        self.head = self.head.right
-        return self.head.val
-        
-        
+        """
+        @return the next smallest number
+        """
+
+        # Node at the top of the stack is the next smallest element
+        topmost_node = self.stack.pop()
+
+        # Need to maintain the invariant. If the node has a right child, call the
+        # helper function for the right child
+        if topmost_node.right:
+            self._leftmost_inorder(topmost_node.right)
+        return topmost_node.val
 
     def hasNext(self) -> bool:
-        if not self.head.right:
-            return False
-        return True
-        
-#Solution 2: Emulate inorder using stack. The next element should always be on the top of the stack. After popping add the left most child of the right child of the popped element.
-# public class BSTIterator {
-
-#     private TreeNode visit;
-#     private Stack<TreeNode> stack;
-    
-#     public BSTIterator(TreeNode root) {
-#         visit = root;
-#         stack = new Stack();
-#     }
-
-#     public boolean hasNext() {
-#         return visit != null || !stack.empty();
-#     }
-
-#     public int next() {
-#         while (visit != null) {
-#             stack.push(visit);
-#             visit = visit.left;
-#         }
-#         TreeNode next = stack.pop();
-#         visit = next.right;
-#         return next.val;
-#     }
-# }
-
-
-
-
-# Your BSTIterator object will be instantiated and called as such:
-# obj = BSTIterator(root)
-# param_1 = obj.next()
-# param_2 = obj.hasNext()
+        """
+        @return whether we have a next smallest number
+        """
+        return len(self.stack) > 0
