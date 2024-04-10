321a8c2f0f83f42d17123e308e83ce8475caac75
ms5914 <55366357+ms5914@users.noreply.github.com>
2024-04-09 17:46:48 -0700

    Time: 41 ms (19.43%), Space: 16.5 MB (45.41%) - LeetHub

--- 0158-read-n-characters-given-read4-ii-call-multiple-times/0158-read-n-characters-given-read4-ii-call-multiple-times.py
+++ 0158-read-n-characters-given-read4-ii-call-multiple-times/0158-read-n-characters-given-read4-ii-call-multiple-times.py
@@ -3,37 +3,29 @@
 
 class Solution:
     def __init__(self):
-        self.buff_internal = [0 for i in range(4)]
-        self.i1, self.n1 = 0,0
+        self.q = deque()
+        
         
     def read(self, buf: List[str], n: int) -> int:
-        i = 0
-        while i<n:
-            if self.i1 >= self.n1:
-                self.i1 = 0
-                self.n1 = read4(self.buff_internal)
-                if self.n1 == 0:
-                    break
-            buf[i] = self.buff_internal[self.i1]
+        buf4 = ["" for i in range(4)]
+        while len(self.q)<n:
+            count_buff = read4(buf4)
+            if count_buff == 0:
+                break
+            for i in range(count_buff):
+                self.q.append(buf4[i])
+                i+=1
+            buf4 = ["" for i in range(4)]
+        
+        i=0
+        while self.q and i<n:
+            ch = self.q.popleft()
+            buf[i] = ch
             i+=1
-            self.i1+=1
         return i
-                
+            
         
         
-    #     public int read(char[] buf, int n) {      // this question is about internal buffer and external buffer
-    # // internal buffer: i4, n4, buf4
-    # // external buffer: i, n, buf
-    #     int i = 0;
-    #     while (i < n) {     // external buf limitation
-    #         if (i4 >= n4) {     // internal buf limitation
-    #             i4 = 0;
-    #             n4 = read4(buf4);
-    #             if (n4 == 0) break;
-    #         }
-    #         buf[i++] = buf4[i4++];
-    #     }
-    #     return i;
-    # }
-    # char[] buf4 = new char[4];
-    # int i4 = 0, n4 = 0;
+
+            
+        
