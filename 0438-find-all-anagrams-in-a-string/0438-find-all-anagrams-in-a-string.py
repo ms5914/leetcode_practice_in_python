35f02a316ed22a476083ef5cbfe975378805fb88
ms5914 <55366357+ms5914@users.noreply.github.com>
2024-04-10 00:18:21 -0700

    Time: 73 ms (90.10%), Space: 17.6 MB (91.02%) - LeetHub

--- 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.py
+++ 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.py
@@ -1,24 +1,28 @@
 class Solution:
     def findAnagrams(self, s: str, p: str) -> List[int]:
-        if len(p)>len(s):
+        ns, np = len(s), len(p)
+        if ns < np:
             return []
 
-        i,j = 0,0
-        t_map = Counter(p)
-        s_map = defaultdict(int)
-        result=[]
-
-        max_match = 0
-        while j<len(s):
-            s_map[s[j]]+=1
-            if j-i+1 == len(p):
-                if s_map == t_map:
-                    result.append(i)
-                s_map[s[i]]-=1
-                if s_map[s[i]] == 0:
-                    del s_map[s[i]]
-                i+=1
-            j+=1
-        return result
-
-
+        p_count, s_count = [0] * 26, [0] * 26
+        # build reference array using string p
+        for ch in p:
+            p_count[ord(ch) - ord('a')] += 1
+        
+        output = []
+        # sliding window on the string s
+        for i in range(ns):
+            # add one more letter 
+            # on the right side of the window
+            s_count[ord(s[i]) - ord('a')] += 1
+            # remove one letter 
+            # from the left side of the window
+            if i >= np:
+                s_count[ord(s[i - np]) - ord('a')] -= 1
+            # compare array in the sliding window
+            # with the reference array
+            if p_count == s_count:
+                output.append(i - np + 1)
+        
+        return output
+        
