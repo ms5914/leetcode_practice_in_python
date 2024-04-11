ac00379449ac1c16ffd08bdac2fc12f93ec97d07
ms5914 <55366357+ms5914@users.noreply.github.com>
2024-04-11 13:51:42 -0700

    Time: 234 ms (62.91%), Space: 25.3 MB (65.66%) - LeetHub

--- 0895-maximum-frequency-stack/0895-maximum-frequency-stack.py
+++ 0895-maximum-frequency-stack/0895-maximum-frequency-stack.py
@@ -1,59 +1,21 @@
-from collections import defaultdict
-class FreqStack:
+class FreqStack(object):
 
     def __init__(self):
-        self.mapp = defaultdict(list)
-        self.freq_map = defaultdict(int)
-        self.max_freq = 0
-        
-    def push(self, val: int) -> None:
-        self.freq_map[val]+=1
-        self.max_freq = max( self.max_freq, self.freq_map[val])
-        self.mapp[self.freq_map[val]].append(val)
+        self.freq = collections.Counter()
+        self.group = collections.defaultdict(list)
+        self.maxfreq = 0
 
+    def push(self, x):
+        f = self.freq[x] + 1
+        self.freq[x] = f
+        if f > self.maxfreq:
+            self.maxfreq = f
+        self.group[f].append(x)
 
-    def pop(self) -> int:
-        ele = self.mapp[self.max_freq].pop()
-        self.freq_map[ele]-=1
-        if not self.mapp[self.max_freq]:
-            self.max_freq-=1
-        return ele
-    
-    
-# import heapq
+    def pop(self):
+        x = self.group[self.maxfreq].pop()
+        self.freq[x] -= 1
+        if not self.group[self.maxfreq]:
+            self.maxfreq -= 1
 
-# class FreqStack:
-
-#     def __init__(self):
-#         self.freq_map = {} #To track latest freq of an element (after pushes and pops and then accordingly add to heap)
-#         self.heap = []  #This heap has (freq, time, elem). There are multiple entries of the same element in this heap. Freq and time is how we decide what to pop. 
-#         self.t = 0   #To track which elem is closest to the top kinda like time of push
-        
-    
-#     def push(self, val: int) -> None:
-        
-#         if val in self.freq_map:
-#             self.freq_map[val]+=1
-#         else:
-#             self.freq_map[val] = 1 
-            
-        
-#         self.t +=1
-#         heapq.heappush(self.heap, (-self.freq_map[val], -self.t, val))    
-        
-
-#     def pop(self) -> int:
-#         _, _, val = heapq.heappop(self.heap)
-#         self.freq_map[val]-=1
-#         return val
-        
-        
-        
-        
-        
-
-
-# Your FreqStack object will be instantiated and called as such:
-# obj = FreqStack()
-# obj.push(val)
-# param_2 = obj.pop()
+        return x
