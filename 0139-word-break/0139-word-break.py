7dc58834c623599e471009e3cd036b89f7ce8e7a
ms5914 <55366357+ms5914@users.noreply.github.com>
2024-04-10 23:19:46 -0700

    Time: 29 ms (97.41%), Space: 17.7 MB (5.48%) - LeetHub

--- 0139-word-break/0139-word-break.py
+++ 0139-word-break/0139-word-break.py
@@ -1,17 +1,33 @@
-class Solution:      
-    def wordBreak(self, s: str, wordDict: List[str]) -> bool:
-        
-        
-        @lru_cache(maxsize=None)
-        def helper(index):
-            if index==len(s):
-                return True
+class TrieNode:
+    def __init__(self):
+        self.is_word = False
+        self.children = {}
 
-            for i in range(index+1, len(s)+1):
-                if s[index:i] in wordDict:
-                    if helper(i):
-                        return True
-            return False
-        
-        return helper(0)
+class Solution:
+    def wordBreak(self, s: str, wordDict: List[str]) -> bool:
+        root = TrieNode()
+        for word in wordDict:
+            curr = root
+            for c in word:
+                if c not in curr.children:
+                    curr.children[c] = TrieNode()
+                curr = curr.children[c]
             
+            curr.is_word = True
+            
+        dp = [False] * len(s)
+        for i in range(len(s)):
+            if i == 0 or dp[i - 1]:
+                curr = root
+                for j in range(i, len(s)):
+                    c = s[j]
+                    if c not in curr.children:
+                        # No words exist
+                        break
+
+                    curr = curr.children[c]
+                    if curr.is_word:
+                        dp[j] = True
+
+        return dp[-1]
+        
