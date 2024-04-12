c7239ef936f98c31ad5da1acd0912e17f7eccbf2
ms5914 <55366357+ms5914@users.noreply.github.com>
2024-04-11 17:09:51 -0700

    Time: 88 ms (64.11%), Space: 21.2 MB (41.02%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -36,6 +36,16 @@ class Solution:
         quick_select(nums, 0, len(nums)-1)
         
         return [num[0] for num in nums[n-k:]]
+    
+    
+    
+    #Can be done using heaps / Quick select and the easiest one and most efficient is using bucket sort. 
+        
+        # bucket = [[] for _ in range(len(nums) + 1)]
+        # Count = Counter(nums).items()  
+        # for num, freq in Count: bucket[freq].append(num) 
+        # flat_list = [item for sublist in bucket for item in sublist]
+        # return flat_list[::-1][:k]
             
             
             
