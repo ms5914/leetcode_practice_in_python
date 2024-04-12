b4744338be9c3abb6f487090b540f037b4b6f41f
ms5914 <55366357+ms5914@users.noreply.github.com>
2024-04-11 18:31:34 -0700

    Time: 437 ms (61.61%), Space: 17 MB (93.59%) - LeetHub

--- 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.py
+++ 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.py
@@ -1,18 +1,17 @@
-from collections import deque
 class Solution:
     def longestOnes(self, nums: List[int], k: int) -> int:
-        
-        dq = deque()
-        zeros = 0
+        left = 0
         ans = 0
-        for i in range(len(nums)):
-            dq.append(nums[i])
-            if not nums[i]:
-                zeros+=1
-                
-            while zeros>k and dq:
-                element = dq.popleft()
-                if element==0:
-                    zeros-=1
-            ans = max(ans, len(dq))
+        for right in range(len(nums)):
+            # If we included a zero in the window we reduce the value of k.
+            # Since k is the maximum zeros allowed in a window.
+            k -= 1 - nums[right]
+            # A negative k denotes we have consumed all allowed flips and window has
+            # more than allowed zeros, thus increment left pointer by 1 to keep the window size same.
+            while k < 0 and left<=right:
+                # If the left element to be thrown out is zero we increase k.
+                k += 1 - nums[left]
+                left += 1
+            ans = max(ans, right-left+1)
+            
         return ans
